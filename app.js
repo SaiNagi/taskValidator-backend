2b5b2a28c7b8e6a92c435f191eb6be1fee503cab
SaiNagi <131807462+SaiNagi@users.noreply.github.com>
2024-12-18 18:30:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ app.get("/tasks", authenticate, (req, res) => {
   const username = req.user.username;
 
   db.all(
-    "SELECT * FROM tasks WHERE assignee = ? OR creator = ? AND WHERE status = 'Pending'",
+    "SELECT * FROM tasks WHERE assignee = ? OR creator = ?",
     [username, username],
     (err, rows) => {
       if (err) res.status(500).json({ message: "Failed to fetch tasks." });
